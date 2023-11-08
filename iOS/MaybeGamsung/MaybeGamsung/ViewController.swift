c15b4e50f281a9fe2d0f1fc727148acdcd86dad4
Junnos  <junnos.dev@gmail.com>
2023-11-08 18:08:33 +0900

    :sparkles: iOS 프로젝트 생성

---
+++ iOS/MaybeGamsung/MaybeGamsung/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaybeGamsung
+//
+//  Created by 이창준 on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
