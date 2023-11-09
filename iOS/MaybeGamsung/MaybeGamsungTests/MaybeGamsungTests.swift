aeef829eb53a94e902a4dd4bb2e1a91261ad4301
Junnos  <junnos.dev@gmail.com>
2023-11-09 20:20:45 +0900

    :construction_worker: Build & Test CI 추가

---
+++ iOS/MaybeGamsung/MaybeGamsungTests/MaybeGamsungTests.swift
@@ -0,0 +1,12 @@
+//
+//  MaybeGamsungTests.swift
+//  MaybeGamsungTests
+//
+//  Created by 이창준 on 11/9/23.
+//
+
+import XCTest
+
+final class MaybeGamsungTests: XCTestCase {
+    func testExample() throws {}
+}
