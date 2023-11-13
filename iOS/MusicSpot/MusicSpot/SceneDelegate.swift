06eda98c8c3f15e41642838f9c91bcba87ef4de1
Junnos  <junnos.dev@gmail.com>
2023-11-13 21:41:46 +0900

    :truck: 프로젝트 생성

---
+++ iOS/MusicSpot/MusicSpot/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  MusicSpot
+//
+//  Created by 이창준 on 11/13/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+}
