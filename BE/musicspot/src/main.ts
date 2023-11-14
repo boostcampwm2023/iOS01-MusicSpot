d0389a9f16efd7cbf128f8d412d15ffa5d5b6089
twoo1999 <twoo19999@gmail.com>
2023-11-14 12:57:41 +0900

    swagger 설정 (#60)

--- BE/musicspot/src/main.ts
+++ BE/musicspot/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example') // 문서의 제목
+    .setDescription('The cats API description') // 문서의 간단한 설명
+    .setVersion('1.0') // API의 버전(업데이트 버전)
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
