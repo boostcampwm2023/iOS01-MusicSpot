dc2e3f197f1bf07e3a086a1f61c154063e6aa311
mingun <pushedGun@github.com>
2023-11-15 11:19:46 +0900

    :art: swiftlint, swiftformat 환경셋팅 및 규칙에 맞게 코드 정리

--- iOS/MusicSpot/MusicSpot/AppDelegate.swift
+++ iOS/MusicSpot/MusicSpot/AppDelegate.swift
@@ -9,18 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        true
     }
-    
+
     // MARK: UISceneSession Lifecycle
-    
-    func application(_ application: UIApplication,
+
+    func application(_: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
-                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+                     options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    
 }
--- iOS/MusicSpot/MusicSpot/SceneDelegate.swift
+++ iOS/MusicSpot/MusicSpot/SceneDelegate.swift
@@ -8,14 +8,11 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
     var window: UIWindow?
-    
+
     func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
-        
+               willConnectTo _: UISceneSession,
+               options _: UIScene.ConnectionOptions) {
+        guard (scene as? UIWindowScene) != nil else { return }
     }
-    
 }
