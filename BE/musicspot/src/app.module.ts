7e94dc5d96766823076ab089d36e273ba2e711cc
twoo1999 <twoo19999@gmail.com>
2023-11-15 22:44:47 +0900

    :sparkles: MongoDB 설정
    MongoDB 설정입니다.

--- BE/musicspot/src/app.module.ts
+++ BE/musicspot/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { JournalsModule } from './journals/journals.module';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://192.168.174.128:27017/musicspotDB'),
+    JournalsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
