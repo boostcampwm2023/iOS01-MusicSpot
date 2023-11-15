6f6bee6a621bd2775881250870a94905d3e6f2b7
twoo1999 <twoo19999@gmail.com>
2023-11-16 02:39:39 +0900

    :sparkles: Journal 추가 기능 추가
    journal 시작 시 새로운 journal을 db에 추가하고 추가된 journal을 클라이언트로 응답

---
+++ BE/musicspot/src/journals/journals.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type JournalDocument = HydratedDocument<Journal>;
+
+@Schema()
+export class Journal {
+  @Prop()
+  title: string;
+
+  @Prop()
+  spots: string[];
+
+  @Prop({ type: [[Number]] })
+  coordinates: number[][];
+}
+
+export const JournalSchema = SchemaFactory.createForClass(Journal);
