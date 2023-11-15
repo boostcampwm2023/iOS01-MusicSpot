fe460570a465f56c879a9d36316411d77e98ff7c
twoo1999 <twoo19999@gmail.com>
2023-11-16 02:37:31 +0900

    :art: 잡다한 코드 수정
    테스트 및 의미없는 부분 수정

--- BE/musicspot/src/app.module.ts
+++ BE/musicspot/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JournalsModule } from './journals/journals.module';
+import { JournalsController } from './journals/journals.controller';
+import { JournalsService } from './journals/journals.service';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://192.168.174.128:27017/musicspotDB'),
