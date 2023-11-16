725c30228ef69dddaa7b87c89c019c98d601976b
twoo1999 <twoo19999@gmail.com>
2023-11-16 18:03:57 +0900

    :sparkles: Journal 아이디 추가 구현
    여정 시작 시 journal id를 유저에 추가하는 기능 구현

--- BE/musicspot/src/journals/journals.service.ts
+++ BE/musicspot/src/journals/journals.service.ts
@@ -3,11 +3,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Injectable } from '@nestjs/common';
 import { StartJournalDTO } from './dto/journalStart.dto';
 import { Journal } from './journals.schema';
+import { PersonService } from '../person/person.service';
 
 @Injectable()
 export class JournalsService {
   constructor(
     @InjectModel(Journal.name) private journalModel: Model<Journal>,
+    private personService: PersonService,
   ) {}
 
   async create(startJournalDTO: StartJournalDTO): Promise<Journal> {
@@ -17,6 +19,12 @@ export class JournalsService {
       coordinates: [startJournalDTO.coordinate],
     };
     const createdJournal = new this.journalModel(journalData);
-    return createdJournal.save();
+    const returnData = await createdJournal.save();
+    const journalId = returnData._id;
+    this.personService.appendJournalIdToPerson(
+      startJournalDTO.email,
+      journalId,
+    );
+    return returnData;
   }
 }
