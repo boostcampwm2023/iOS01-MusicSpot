5a1bf8ecc197a4fbfc8696f6ac5f8eb3eb47fc3f
twoo1999 <twoo19999@gmail.com>
2023-11-16 18:05:38 +0900

    :art: 의존성 추가를 위한 작업
    import에 personService를 추가해서 의존성 주입

--- BE/musicspot/src/journals/journals.module.ts
+++ BE/musicspot/src/journals/journals.module.ts
@@ -3,12 +3,14 @@ import { JournalsController } from './journals.controller';
 import { JournalsService } from './journals.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JournalSchema, Journal } from './journals.schema';
+import { PersonService } from 'src/person/person.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Journal.name, schema: JournalSchema }]),
+    PersonService,
   ],
   controllers: [JournalsController],
-  providers: [JournalsService],
+  providers: [JournalsService, PersonService],
 })
 export class JournalsModule {}
