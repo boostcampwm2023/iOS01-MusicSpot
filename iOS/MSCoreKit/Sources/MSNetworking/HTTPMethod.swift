60201b6412e4d52c1c226fcf0cddd3dc06c78b3b
mingun <pushedGun@github.com>
2023-11-17 14:16:15 +0900

    :sparkles: MSNetwork 로직 구현
    MSNetwork 계층화

---
+++ iOS/MSCoreKit/Sources/MSNetworking/HTTPMethod.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPMethod.swift
+//
+//
+//  Created by 전민건 on 11/16/23.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case put = "PUT"
+    case delete = "DELETE"
+    
+}
