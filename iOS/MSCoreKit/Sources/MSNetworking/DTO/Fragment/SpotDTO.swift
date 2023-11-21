f508f2bc05321d6162a4caffabaa2a0fbf34551a
전민건 <111111595+PushedGun@users.noreply.github.com>
2023-11-21 13:36:24 +0900

    Update iOS/MSCoreKit/Sources/MSNetworking/DTO/Fragment/SpotDTO.swift
    Co-authored-by: 윤동주 <54929503+yoondj98@users.noreply.github.com>

--- iOS/MSCoreKit/Sources/MSNetworking/DTO/Fragment/SpotDTO.swift
+++ iOS/MSCoreKit/Sources/MSNetworking/DTO/Fragment/SpotDTO.swift
@@ -10,7 +10,7 @@ import Foundation
 struct SpotDTO: Codable {
     
     let spotIdentifier: UUID
-    let cooredinate: [[Double]]
+    let coordinate: [Double]
     let photo: Data?
     let w3w: String
     
