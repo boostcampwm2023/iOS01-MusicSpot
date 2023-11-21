0ebf228b511f40d70e470088a055c461c4a0876b
twoo1999 <twoo19999@gmail.com>
2023-11-22 03:57:27 +0900

    :package: Create 메서드 분리
    create 메서드 내부 로직을 모듈화

--- BE/musicspot/src/spot/spot.service.ts
+++ BE/musicspot/src/spot/spot.service.ts
@@ -4,7 +4,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Spot } from './spot.schema';
 import { RecordSpotDTO } from './dto/recordSpot.dto';
-import { Journey } from 'src/journey/journey.schema';
+import { Journey } from '../journey/journey.schema';
+import * as AWS from 'aws-sdk';
+import fs from 'fs';
+const endpoint = 'https://kr.object.ncloudstorage.com';
+const region = 'kr-standard';
+const access_key = '194C0D972294FBAFCE35';
+const secret_key = '1F6B29EFE66643E13693CE8D23AFD8897D8A1951';
 
 @Injectable()
 export class SpotService {
@@ -12,14 +18,36 @@ export class SpotService {
     @InjectModel(Spot.name) private spotModel: Model<Spot>,
     @InjectModel(Journey.name) private journeyModel: Model<Journey>,
   ) {}
+  async uploadPhotoToStorage(photoData: Buffer): Promise<string> {
+    const S3 = new AWS.S3({
+      endpoint,
+      region,
+      credentials: {
+        accessKeyId: access_key,
+        secretAccessKey: secret_key,
+      },
+    });
+    const key = `${Date.now()}`;
+    const result = await S3.putObject({
+      Bucket: 'music-spot-storage',
+      Key: key,
+      Body: photoData,
+    }).promise();
+    console.log(result);
 
-  async create(recordSpotDTO: RecordSpotDTO): Promise<Spot> {
-    const createdSpotData = await new this.spotModel(recordSpotDTO).save();
+    return `https://kr.object.ncloudstorage.com/music-spot-storage/${key}`;
+  }
+  async insertToSpot(spotData) {
+    const createdSpotData = await new this.spotModel(spotData).save();
     const spotId = createdSpotData._id;
     await this.journeyModel.updateOne(
-      { _id: recordSpotDTO.journeyId },
+      { _id: spotData.journeyId },
       { $push: { spots: spotId } },
     );
     return createdSpotData;
+  }
+  async create(recordSpotDto: RecordSpotDTO) {
+    const photoUrl = await this.uploadPhotoToStorage(recordSpotDto.photoData);
+    return await this.insertToSpot({ ...recordSpotDto, photoUrl });
   }
 }
