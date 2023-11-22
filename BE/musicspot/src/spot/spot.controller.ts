5cbfb798b54d9de5c6aa48e826b5adf2ac9b93d7
twoo1999 <twoo19999@gmail.com>
2023-11-22 22:37:27 +0900

    :sparkles: swagger api 설명 추가

--- BE/musicspot/src/spot/spot.controller.ts
+++ BE/musicspot/src/spot/spot.controller.ts
@@ -5,14 +5,25 @@ import {
   UseInterceptors,
   UploadedFile,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RecordSpotDTO } from './dto/recordSpot.dto';
 import { SpotService } from './spot.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Spot } from './spot.schema';
 @Controller('spot')
 @ApiTags('spot 관련 API')
 export class SpotController {
   constructor(private spotService: SpotService) {}
+
+  @ApiOperation({
+    summary: 'spot 기록 시 실행되는 API',
+    description:
+      'request로 여정 ID(string), 위치좌표([number, number]), timestamp(string), 유저 이메일(string)을 필요합니다.',
+  })
+  @ApiCreatedResponse({
+    description: '생성된 spot 데이터를 반환',
+    type: Spot,
+  })
   @Post()
   async create(@Body() recordSpotDTO: RecordSpotDTO) {
     return await this.spotService.create(recordSpotDTO);
