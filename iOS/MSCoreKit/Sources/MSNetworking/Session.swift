07469c940703141a778cf697bd4722df892de372
전민건 <111111595+PushedGun@users.noreply.github.com>
2023-11-24 00:19:12 +0900

    [iOS] MSNetworking 모듈 추가 (#69)
    * :sparkles: MSLogger 구현
    
    MSLogger, MSLog에 사용될 category 구현
    
    * :test_tube: MSLogger Test 코드 추가
    
    .
    
    * :memo: 누락된 문서 추가
    
    누락된 문서 추가
    
    * :truck: DTO 생성 및 경로 분리
    
    DTO 생성 및 경로 분리
    
    * :truck: DTO 파일 생성
    
    DTO 파일 생성
    
    * :sparkles: MSNetwork 로직 구현
    
    MSNetwork 계층화
    
    * :test_tube: 테스트 코드 추가
    
    테스트 코드 추가(추후 수정 필요)
    
    * :art: 프로젝트 설정 변경
    
    프로젝트 설정 변경
    
    * :memo: .gitignore 파일 수정
    
    ../.DS_Store 파일 저장하지 않도록 추가
    
    ---------
    
    Co-authored-by: mingun <pushedGun@github.com>

---
+++ iOS/MSCoreKit/Sources/MSNetworking/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  MSCoreKit
+//
+//  Created by 전민건 on 11/16/23.
+//
+
+import Foundation
+
+protocol Session {
+    
+    func dataTaskPublisher(for request: URLRequest) -> URLSession.DataTaskPublisher
+    
+}
+
+extension URLSession: Session {
+    
+}
