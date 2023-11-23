ada194e15b1d00afbe9b3996210fae07b70b2374
twoo1999 <twoo19999@gmail.com>
2023-11-24 03:24:15 +0900

    :sparkles: 배포를 위한 페이지 구현
    배포 시 사용하는 페이지를 만듬

--- BE/musicspot/src/main.ts
+++ BE/musicspot/src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Music Spot') // 문서의 제목
     .setDescription('iOS01 Music Spot App API') // 문서의 간단한 설명
