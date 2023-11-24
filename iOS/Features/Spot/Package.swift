e2d7fb586eb15f3f2d5df0b39e52b531ceeca11a
mingun <pushedGun@github.com>
2023-11-24 20:48:01 +0900

    :memo: Package 파일 생성

---
+++ iOS/Features/Spot/Package.swift
@@ -0,0 +1,53 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// MARK: - Constants
+
+extension String {
+    static let package = "Spot"
+    static let spotView = "SpotView"
+    static let msUIKit = "MSUIKit"
+    static let msFoundation = "MSFoundation"
+    static let msDesignsystem = "MSDesignSystem"
+    static let msLogger = "MSLogger"
+    
+    var testTarget: String {
+        return self + "Tests"
+    }
+    
+    var path: String {
+        return "../../" + self
+    }
+    
+}
+
+// MARK: - Package
+
+let package = Package(
+    name: .package,
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: .spotView,
+            targets: [.spotView]),
+    ],
+    dependencies: [
+        .package(path: .msUIKit.path),
+        .package(path: .msFoundation.path)
+    ],
+    targets: [
+        // Codes
+        .target(
+            name: .spotView,
+            dependencies: [
+                .product(name: .msUIKit, package: .msUIKit),
+                .product(name: .msDesignsystem, package: .msUIKit),
+                .product(name: .msLogger, package: .msFoundation)]),
+        
+        // Tests
+    ]
+)
