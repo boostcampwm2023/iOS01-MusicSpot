2237f0a2ec75470cfd38b8b39d95af0d1156ff7d
Junnos  <junnos.dev@gmail.com>
2023-11-28 15:34:34 +0900

    :package: Workspace 세팅, Feature 패키지 생성

---
+++ iOS/Features/RewindJourney/Package.swift
@@ -0,0 +1,56 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// MARK: - Constants
+
+extension String {
+    static let package = "RewindJourney"
+    static let rewindJourneyView = "RewindJourneyView"
+    static let msUIKit = "MSUIKit"
+    static let msFoundation = "MSFoundation"
+    static let msDesignsystem = "MSDesignSystem"
+    static let msLogger = "MSLogger"
+    
+    var testTarget: String {
+        return self + "Tests"
+    }
+    
+    var path: String {
+        return "../../" + self
+    }
+    
+    var featurePath: String {
+        return "../Features/" + self
+    }
+    
+}
+
+// MARK: - Package
+
+let package = Package(
+    name: .package,
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: .rewindJourneyView,
+                 targets: [.rewindJourneyView])
+    ],
+    dependencies: [
+        .package(path: .msUIKit.path),
+        .package(path: .msFoundation.path)
+    ],
+    targets: [
+        // Codes
+        .target(name: .rewindJourneyView,
+                dependencies: [
+                    .product(name: .msUIKit, package: .msUIKit),
+                    .product(name: .msDesignsystem, package: .msUIKit),
+                    .product(name: .msLogger, package: .msFoundation)
+                ])
+        
+        // Tests
+    ]
+)
---
+++ iOS/Features/SelectSong/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SelectSong",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "SelectSong",
+                 targets: ["SelectSong"])
+    ],
+    targets: [
+        .target(name: "SelectSong"),
+        .testTarget(name: "SelectSongTests",
+                    dependencies: ["SelectSong"])
+    ]
+)
