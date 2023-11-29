9c1fcb294c8a242061948174d1f14aafb0417211
Junnos  <junnos.dev@gmail.com>
2023-11-29 23:30:15 +0900

    :test_tube: MSCacheStorage 테스트 케이스 작성

--- iOS/MSCoreKit/Tests/MSCacheStorageTests/MSCacheStorageTests.swift
+++ iOS/MSCoreKit/Tests/MSCacheStorageTests/MSCacheStorageTests.swift
@@ -10,4 +10,68 @@ import XCTest
 @testable import MSCacheStorage
 
 final class MSCacheStorageTests: XCTestCase {
+    
+    // MARK: - Properties
+    
+    private var cacheStorage: MSCacheStorage!
+    private var mockData = "TESTVALUE"
+    private let key = "cacheKey"
+    
+    // MARK: - Setup
+    
+    override func setUp() {
+        let memory = CacheStorage.Cache()
+        let disk = FileManager.default
+        self.cacheStorage = MSCacheStorage(cache: memory, fileManager: disk)
+        self.cacheStorage.remove(forKey: self.key)
+    }
+    
+    override func tearDown() {
+        self.cacheStorage = nil
+    }
+    
+    // MARK: - Tests
+    
+    func test_새로운Key값으로_캐싱_성공() {
+        let sut = Data(self.mockData.utf8)
+        
+        let result = self.cacheStorage.cache(sut, forKey: self.key)
+        XCTAssertEqual(result, .success,
+                       "새로운 Key 값으로 저장한 값은 .success 결과를 반환해야 합니다.")
+    }
+    
+    func test_중복된Key값으로_캐싱_성공() {
+        let sut = Data(self.mockData.utf8)
+        let sut2 = Data("AnoterTest".utf8)
+        
+        self.cacheStorage.cache(sut, forKey: self.key)
+        let result = self.cacheStorage.cache(sut2, forKey: self.key)
+        XCTAssertEqual(result, .success,
+                       "중복된 Key 값으로 저장할 경우, 이전 캐싱된 값을 대체하며 성공해야 합니다.")
+    }
+    
+    func test_존재하는Key값으로_캐시데이터조회_데이터반환_성공() throws {
+        let sut = Data(self.mockData.utf8)
+        
+        let result = self.cacheStorage.cache(sut, forKey: self.key)
+        print(result)
+        guard let cachedValue = self.cacheStorage.data(forKey: self.key) else {
+            XCTFail("캐싱된 데이터로부터 nil 값이 반환 되었습니다.")
+            return
+        }
+        
+        let decodedData = String(data: cachedValue, encoding: .utf8)
+        XCTAssertEqual(decodedData, self.mockData,
+                       "캐싱한 값과 불러온 값이 일치하지 않습니다.")
+    }
+    
+    func test_존재하지않는Key값으로_캐시데이터조회_nil반환_성공() {
+        let sut = Data(self.mockData.utf8)
+        
+        self.cacheStorage.cache(sut, forKey: self.key)
+        let nilValue = self.cacheStorage.data(forKey: "undefined key")
+        
+        XCTAssertNil(nilValue, "존재하지 않는 key 값은 nil 값을 반환해야 합니다.")
+    }
+    
 }
