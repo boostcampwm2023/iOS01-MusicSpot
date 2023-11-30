2315684977eaed1eed6cc5195236bcb0565f8d05
mingun <pushedGun@github.com>
2023-11-30 14:20:48 +0900

    :memo: 이미지 송출해주는 로직을 다룰 viewModel 파일 생성

---
+++ iOS/Features/Spot/Sources/Spot/SpotSaveViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  SpotSaveViewModel.swift
+//  Spot
+//
+//  Created by 전민건 on 11/29/23.
+//
+
+import Foundation
+
+final class SpotSaveViewModel {
+    
+    // MARK: - Properties
+    
+}
+
+// MARK: - Interface
+
+internal extension SpotSaveViewModel {
+    
+    func upload(data: Data) {
+        // Networking..
+    }
+    
+}
