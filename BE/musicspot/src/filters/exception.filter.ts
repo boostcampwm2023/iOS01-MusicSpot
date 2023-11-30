352154017d0adf1af2fe3c5831c59362f24c1eba
taewoo <twoo19999@gmail.com>
2023-11-30 15:52:38 +0900

    exception filter 수정

--- BE/musicspot/src/filters/exception.filter.ts
+++ BE/musicspot/src/filters/exception.filter.ts
@@ -8,21 +8,25 @@ export class AllExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const req = ctx.getRequest();
     const res = ctx.getResponse();
+    const err = exception;
 
-    const err =
-      exception instanceof BaseException
-        ? exception
-        : new UnexpectedException();
-
-    err.timestamp = new Date().toISOString();
-    err.path = req.url;
-
-    res.status(err.statusCode).json({
-      errorCode: err.errorCode,
-      statusCode: err.statusCode,
-      timestamp: err.timestamp,
-      path: err.path,
-      message: err.message,
-    });
+    if (err instanceof BaseException) {
+      err.timestamp = new Date().toISOString();
+      err.path = req.url;
+      res.status(err.statusCode).json({
+        errorCode: err.errorCode,
+        statusCode: err.statusCode,
+        timestamp: err.timestamp,
+        path: err.path,
+        message: err.message,
+      });
+    } else {
+      res.status(err.status).json({
+        statusCode: err.status,
+        timestamp: new Date().toISOString(),
+        path: req.url,
+        message: err.response.message,
+      });
+    }
   }
 }
