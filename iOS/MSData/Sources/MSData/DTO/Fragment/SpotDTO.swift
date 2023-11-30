3e43c97d5d2cbbf463f731376daf808a992ed6b7
mingun <pushedGun@github.com>
2023-11-30 17:26:08 +0900

    :art: SpotDTO 생성자 접근 제한 수정

--- iOS/MSData/Sources/MSData/DTO/Fragment/SpotDTO.swift
+++ iOS/MSData/Sources/MSData/DTO/Fragment/SpotDTO.swift
@@ -13,6 +13,12 @@ public struct RequestableSpotDTO: Encodable, Identifiable {
     public let coordinate: [Double]
     public let photoData: Data
     
+    public init(id: UUID, coordinate: [Double], photoData: Data) {
+        self.id = id
+        self.coordinate = coordinate
+        self.photoData = photoData
+    }
+    
 }
 
 public struct ResponsibleSpotDTO: Decodable, Identifiable {
@@ -20,5 +26,11 @@ public struct ResponsibleSpotDTO: Decodable, Identifiable {
     public let id: UUID
     public let coordinate: [Double]
     public let photoURLs: [String]
+    
+    public init(id: UUID, coordinate: [Double], photoURLs: [String]) {
+        self.id = id
+        self.coordinate = coordinate
+        self.photoURLs = photoURLs
+    }
     
 }
