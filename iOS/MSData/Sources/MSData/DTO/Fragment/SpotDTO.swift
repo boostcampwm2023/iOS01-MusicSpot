4b841ce9d45c3aa6f5489a65c3797b9f3bc3b2f4
mingun <pushedGun@github.com>
2023-11-30 17:19:55 +0900

    :art: SpotDTO, 보내는 용과 받는 용으로 타입 분리

--- iOS/MSData/Sources/MSData/DTO/Fragment/SpotDTO.swift
+++ iOS/MSData/Sources/MSData/DTO/Fragment/SpotDTO.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
-public struct SpotDTO: Codable, Identifiable {
+public struct RequestableSpotDTO: Encodable, Identifiable {
+    
+    public let id: UUID
+    public let coordinate: [Double]
+    public let photoData: Data
+    
+}
+
+public struct ResponsibleSpotDTO: Decodable, Identifiable {
     
     public let id: UUID
     public let coordinate: [Double]
