4e86c80911cd641fff29ae7c4d1ab3f2aed81f68
twoo1999 <twoo19999@gmail.com>
2023-12-04 18:21:59 +0900

    :sparkles: User 생성 비지니스 로직 추가
    처음 시작 시 user를 생성하는 기능 추가, user가 기존에 있을 시 중복 예외 처리 추가

--- BE/musicspot/src/user/controller/user.controller.ts
+++ BE/musicspot/src/user/controller/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Body, Post } from '@nestjs/common';
+import { UserService } from '../serivce/user.service';
+import { CreateUserDTO } from '../dto/createUser.dto';
+import { User } from '../schema/user.schema';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 @Controller('user')
-export class UserController {}
+@ApiTags('user 관련 API')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @ApiOperation({
+    summary: '유저 생성 API',
+    description: '첫 시작 시 유저를 생성합니다.',
+  })
+  @ApiCreatedResponse({
+    description: '생성된 유저 데이터를 반환',
+    type: User,
+  })
+  @Post()
+  async create(@Body() createUserDto: CreateUserDTO): Promise<User> {
+    return await this.userService.create(createUserDto);
+  }
+}
