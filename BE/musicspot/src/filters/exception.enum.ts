22a9cc67ed0c86fe432f394c2fdc80079ef7d115
twoo1999 <twoo19999@gmail.com>
2023-12-06 00:31:55 +0900

    [BE] ✨ : 유저 추가 api 구현  (#205)
    * swagger 설정
    
    * :recycle: User 스키마 및 create dto 생성
    
    user 스키마: userId 추가 및 email, nickname 삭제
    
    * :recycle: User 관련 예외 처리 추가
    
    user 생성 시 기존에 있는 id와 중복 시 에러 처리
    
    * :sparkles: User 생성 비지니스 로직 추가
    
    처음 시작 시 user를 생성하는 기능 추가, user가 기존에 있을 시 중복 예외 처리 추가
    
    * :recycle: Filter 수정
    
    base exeption을 상속받는 에러의 경우 statusCode가 출력되지 않는 상황 수정
    
    * :test_tube: 테스트 코드작성(미완성)
    
    user 관련 테스트 코드를 작성 중 new userModel(data) 에 대한 부분을 mocking하는 법을 몰라 테스트 코드 작성 중지

--- BE/musicspot/src/filters/exception.enum.ts
+++ BE/musicspot/src/filters/exception.enum.ts
@@ -7,15 +7,7 @@ export enum JourneyExceptionMessageEnum {
   JourneyNotFound = 'journeyId에 해당하는 Journey가 없습니다.',
   UnexpectedError = '예상치 못한 오류입니다.',
 }
-export enum UserExceptionCodeEnum {
-  UserNotFound = '0002',
-  UnexpectedError = '9998',
-}
 
-export enum UserExceptionMessageEnum {
-  UserNotFound = 'UserId에 해당하는 User가 없습니다.',
-  UnexpectedError = '예상치 못한 오류입니다.',
-}
 export enum SpotExceptionCodeEnum {
   SpotNotFound = '0003',
   UnexpectedError = '9997',
@@ -24,4 +16,13 @@ export enum SpotExceptionCodeEnum {
 export enum SpotExceptionMessageEnum {
   SpotNotFound = 'SpotId에 해당하는 Spot이 없습니다.',
   UnexpectedError = '예상치 못한 오류입니다.',
+}
+
+export enum UserExceptionMessageEnum {
+  UserAlreadyExist = '이미 존재하는 User Id 입니다.',
+  UserNotFound = 'UserId에 해당하는 User가 없습니다.',
+}
+export enum UserExceptionCodeEnum {
+  UserAlreadyExist = '0010',
+  UserNotFound = '0011',
 }
