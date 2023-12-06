922b10317559a8ad86696f75d9b87cd81ac6b976
twoo1999 <twoo19999@gmail.com>
2023-12-07 01:42:41 +0900

    :recycle: Swagger 응답 예시 수정
    객체를 이용한 객체 응답 예시 적용

--- BE/musicspot/src/journey/dto/journeyEnd/journeyEnd.dto.ts
+++ BE/musicspot/src/journey/dto/journeyEnd/journeyEnd.dto.ts
@@ -49,21 +49,7 @@ export class EndJourneyReqDTO {
   readonly title: string;
 
   @ApiProperty({
-    example: {
-      id: '1',
-      name: '655efda2fdc81cae36d20650',
-      artistName: 'newjeans',
-      artwork: {
-        width: 3000,
-        height: 3000,
-        url: 'https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/0b/b2/52/0bb2524d-ecfc-1bae-9c1e-218c978d7072/Honeymoon_3K.jpg/{w}x{h}bb.jpg',
-        bgColor: '202020',
-        textColor1: 'aea6f6',
-        textColor2: 'b68ef6',
-        textColor3: '918bcb',
-        textColor4: '9878cb',
-      },
-    },
+    type: SongDTO,
     description: '노래 정보',
     required: true,
   })
@@ -110,21 +96,7 @@ export class EndJourneyResDTO {
   readonly numberOfCoordinates: number;
 
   @ApiProperty({
-    example: {
-      id: '1',
-      name: '655efda2fdc81cae36d20650',
-      artistName: 'newjeans',
-      artwork: {
-        width: 3000,
-        height: 3000,
-        url: 'https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/0b/b2/52/0bb2524d-ecfc-1bae-9c1e-218c978d7072/Honeymoon_3K.jpg/{w}x{h}bb.jpg',
-        bgColor: '202020',
-        textColor1: 'aea6f6',
-        textColor2: 'b68ef6',
-        textColor3: '918bcb',
-        textColor4: '9878cb',
-      },
-    },
+    type: SongDTO,
     description: '노래 정보',
     required: true,
   })
