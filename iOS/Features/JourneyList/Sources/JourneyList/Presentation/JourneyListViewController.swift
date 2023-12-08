952f994353ed43e7d9e244e39c9e0455106c57ce
Junnos  <junnos.dev@gmail.com>
2023-12-08 20:36:18 +0900

    :art: 사용하지 않는 코드 삭제

--- iOS/Features/JourneyList/Sources/JourneyList/Presentation/JourneyListViewController.swift
+++ iOS/Features/JourneyList/Sources/JourneyList/Presentation/JourneyListViewController.swift
@@ -167,18 +167,6 @@ extension JourneyListViewController: UICollectionViewDelegate {
         return section
     }
     
-    private func fetchImage(url: URL) async throws -> Data {
-        let request = URLRequest(url: url)
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
-              (200...299).contains(statusCode) else {
-            throw NSError(domain: "fetch error", code: 1004)
-        }
-        
-        return data
-    }
-    
     private func configureDataSource() -> JourneyListDataSource {
         let cellRegistration = JourneyCellRegistration { cell, indexPath, itemIdentifier in
             let cellModel = JourneyCellModel(location: itemIdentifier.title,
