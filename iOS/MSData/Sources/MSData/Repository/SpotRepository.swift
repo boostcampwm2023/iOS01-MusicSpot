358f7c4910c1680849cde411e4f0ac3852f8b9d9
mingun <pushedGun@github.com>
2023-12-08 09:02:22 +0900

    :sparkles: Spot Repository 내 네트워크 업로드, 다운로드 기능 추가

--- iOS/MSData/Sources/MSData/Repository/SpotRepository.swift
+++ iOS/MSData/Sources/MSData/Repository/SpotRepository.swift
@@ -8,24 +8,30 @@
 import Foundation
 
 import MSNetworking
+import MSLogger
 
 public protocol SpotRepository {
     
     func fetchRecordingSpots() async -> Result<[SpotDTO], Error>
-    func upload(spot: CreateSpotRequestDTO) async -> Result<Void, Error>
+    func upload(spot: CreateSpotRequestDTO) async -> Result<SpotDTO, Error>
     
 }
 
 public struct SpotRepositoryImplementation: SpotRepository {
     
+    // MARK: - Properties
+    
+    private let networking: MSNetworking
+    
     // MARK: - Initializer
     
-    public init() { }
+    public init(session: URLSession = URLSession(configuration: .default)) {
+        self.networking = MSNetworking(session: session)
+    }
     
     // MARK: - Functions
     
     public func fetchRecordingSpots() async -> Result<[SpotDTO], Error> {
-        
         #if DEBUG
         guard let jsonURL = Bundle.module.url(forResource: "MockSpot", withExtension: "json") else {
             return .failure((MSNetworkError.invalidRouter))
@@ -38,28 +44,36 @@ public struct SpotRepositoryImplementation: SpotRepository {
         } catch {
             print(error)
         }
-        #else
-        
-        #endif
-        
         return .failure(MSNetworkError.unknownResponse)
-    }
-    
-    public func upload(spot: CreateSpotRequestDTO) async -> Result<Void, Error> {
-        #if DEBUG
-        return .success(())
         #else
-        let router = JourneyRouter.checkJourney(userID: UUID(),
-                                                minCoordinate: CoordinateDTO(minCoordinate),
-                                                maxCoordinate: CoordinateDTO(maxCoordinate))
-        let result = await self.networking.request(CheckJourneyResponseDTO.self, router: router)
+        let router = SpotRouter.downloadSpot
+        let result = await self.networking.request([SpotDTO].self, router: router)
         switch result {
-        case .success(let journeys):
-            return .success(journeys)
+        case .success(let spot):
+            MSLogger.make(category: .network).debug("성공적으로 다운로드하였습니다.")
+            return .success(spot)
         case .failure(let error):
+            MSLogger.make(category: .network).debug("\(error): 다운로드에 실패하였습니다.")
             return .failure(error)
         }
         #endif
+    }
+    
+    public func upload(spot: CreateSpotRequestDTO) async -> Result<SpotDTO, Error> {
+//        #if DEBUG
+//        return .success()
+//        #else
+        let router = SpotRouter.upload(spot: spot, id: UUID())
+        let result = await self.networking.request(SpotDTO.self, router: router)
+        switch result {
+        case .success(let spot):
+            MSLogger.make(category: .network).debug("성공적으로 업로드하였습니다.")
+            return .success(spot)
+        case .failure(let error):
+            MSLogger.make(category: .network).debug("\(error): 업로드에 실패하였습니다.")
+            return .failure(error)
+        }
+//        #endif
     }
             
 }
