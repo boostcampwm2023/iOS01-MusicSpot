7c0ad87b271e6c4c8a6aaec335f895c3adbcd81b
mingun <pushedGun@github.com>
2023-12-10 13:46:42 +0900

    :art: Local repository 추가

--- iOS/MSData/Sources/MSData/Repository/LocalRepository.swift
+++ iOS/MSData/Sources/MSData/Repository/LocalRepository.swift
@@ -1,8 +1,49 @@
 //
-//  File.swift
-//  
+//  LocalRepository.swift
+//  MSData
 //
 //  Created by 전민건 on 12/10/23.
 //
 
 import Foundation
+
+import MSDomain
+import MSPersistentStorage
+
+public protocol LocalRepository {
+
+    var key: String { get }
+    func save(coordinate: Coordinate)
+    func loadCoordinates(using key: String) -> [Coordinate]?
+
+}
+
+public struct LocalRepositoryImplementation: LocalRepository {
+    
+    // MARK: - Properties
+    
+    private var storage = FileManagerStorage()
+    private var coordinateURL: URL?
+    public let key: String
+    
+    // MARK: - Initializer
+    
+    public init() {
+        self.key = UUID().uuidString
+    }
+    
+    // MARK: - Functions
+    
+    public func save(coordinate: Coordinate) {
+        
+    }
+    
+    public func loadCoordinates(using key: String) -> [Coordinate]? {
+        if self.key.isEmpty {
+             return nil
+        } else {
+            return nil
+        }
+    }
+    
+}
