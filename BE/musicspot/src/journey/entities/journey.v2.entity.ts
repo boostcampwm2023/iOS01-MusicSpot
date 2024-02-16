3e3ce7881229b59087fce744d8589f12cd53fba9
twoo1999 <twoo19999@gmail.com>
2024-02-17 03:11:51 +0900

    :sparkles: (V2) photo table 분리에 따른 spot 및 journey entity V2 생성

---
+++ BE/musicspot/src/journey/entities/journey.v2.entity.ts
@@ -0,0 +1,36 @@
+import { UUID } from 'crypto';
+import { SpotV2 } from '../../spot/entities/spot.v2.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+@Entity({ name: 'journey' })
+export class JourneyV2 {
+  @PrimaryGeneratedColumn()
+  journeyId: number;
+
+  @Column({ length: 36 })
+  userId: UUID;
+
+  @Column({ length: 30 })
+  title: string;
+
+  @Column()
+  startTimestamp: string;
+
+  @Column()
+  endTimestamp: string;
+
+  @Column()
+  song: string;
+
+  @Column('geometry')
+  coordinates: string;
+
+  // @OneToMany(()=> Spot, spot => spot.journeyId)
+  @OneToMany(() => SpotV2, (spot) => spot.journey)
+  spots: SpotV2[];
+}
---
+++ BE/musicspot/src/photo/entity/photo.entity.ts
@@ -0,0 +1,23 @@
+import { SpotV2 } from '../../spot/entities/spot.v2.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  Column,
+} from 'typeorm';
+@Entity()
+export class Photo {
+  @PrimaryGeneratedColumn()
+  photoId: number;
+
+  @Column()
+  spotId: number;
+
+  @Column()
+  photoKey: string;
+
+  @ManyToOne(() => SpotV2, (spot) => spot.photos)
+  @JoinColumn({ name: 'spotId' })
+  spot: SpotV2;
+}
