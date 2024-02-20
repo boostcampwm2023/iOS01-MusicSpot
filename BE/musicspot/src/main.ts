ec91dced2fd215096a0bf39530f37329159ed3f4
twoo1999 <twoo19999@gmail.com>
2024-02-21 01:09:47 +0900

    :sparkles: swagger 보안 적용
    express-basic-auth를 통해 swagger api 문서도 아이디와 비밀번호를 통해 접근할 수 있도록 수정

--- BE/musicspot/src/main.ts
+++ BE/musicspot/src/main.ts
@@ -4,12 +4,25 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AllExceptionFilter } from './filters/exception.filter';
+import * as expressBasicAuth from 'express-basic-auth';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  app.use(
+    //이 부분 추가
+    ['/api'], // docs(swagger end point)에 진입시
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD, // 지정된 ID/비밀번호
+      },
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Music Spot') // 문서의 제목
     .setDescription('iOS01 Music Spot App API') // 문서의 간단한 설명
@@ -17,7 +30,11 @@ async function bootstrap() {
     .addTag('Music Spot')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
