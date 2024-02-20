52b4c6baa1ef38fb65adc609df22a34877648d01
Junnos  <junnos.dev@gmail.com>
2024-02-20 16:38:41 +0900

    :bug: DesignSystem에 있던 UIKit, SwiftUI 적용 함수를 각각의 패키지로 분리해 타입 추론이 가능하도록 수정
    UIImage.msIcon ➡️ .msIcon 같이 간단하게 사용할 수 있도록 위함

--- iOS/MSFusion/Sources/MSUIKit/DesignSystem+UIKit/MSFont+UIKit.swift
+++ iOS/MSFusion/Sources/MSUIKit/DesignSystem+UIKit/MSFont+UIKit.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import MSDesignSystem
+
 extension MSFont {
     
     fileprivate func font() -> UIFont? {
---
+++ iOS/MSFusion/Sources/MSUIKit/DesignSystem+UIKit/MSIcon+UIKit.swift
@@ -0,0 +1,19 @@
+//
+//  MSIcon+UIKit.swift
+//  MSDesignSystem
+//
+//  Created by 이창준 on 2024.02.19.
+//
+
+import UIKit
+
+import MSDesignSystem
+
+extension UIImage {
+    
+    public static func msIcon(_ icon: MSIcon) -> UIImage? {
+        return UIImage(named: icon.rawValue, in: .msDesignSystem, compatibleWith: .current)?
+            .withRenderingMode(.alwaysTemplate)
+    }
+    
+}
