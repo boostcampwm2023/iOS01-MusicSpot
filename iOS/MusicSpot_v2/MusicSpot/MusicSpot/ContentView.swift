a6a67c79faff70f90c65c82c2b29ef63daaf91a6
SwiftyJunnos <junnos.dev@gmail.com>
2024-04-15 21:50:29 +0900

    :truck: SwiftUI 프로젝트 생성

---
+++ iOS/MusicSpot_v2/MusicSpot/MusicSpot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicSpot
+//
+//  Created by 이창준 on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS/MusicSpot_v2/MusicSpot/MusicSpot/MusicSpotApp.swift
@@ -0,0 +1,19 @@
+//
+//  MusicSpotApp.swift
+//  MusicSpot
+//
+//  Created by 이창준 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicSpotApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+}
