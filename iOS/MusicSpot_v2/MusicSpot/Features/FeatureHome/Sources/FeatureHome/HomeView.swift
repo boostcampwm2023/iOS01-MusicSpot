575c09cad389738ef1da084e1311735d461bbd1e
SwiftyJunnos <junnos.dev@gmail.com>
2024-04-15 22:49:25 +0900

    :sparkles: Router 추가

---
+++ iOS/MusicSpot_v2/MusicSpot/Features/FeatureHome/Sources/FeatureHome/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  FeatureHome
+//
+//  Created by 이창준 on 4/15/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ iOS/MusicSpot_v2/MusicSpot/MSFusion/Sources/MSSwiftUI/Router/Router+Destination.swift
@@ -0,0 +1,15 @@
+//
+//  Router+Destination.swift
+//  MSSwiftUI
+//
+//  Created by 이창준 on 4/15/24.
+//
+
+extension Router {
+    
+    /// 목적지 목록. `Hashable`해야 합니다.
+    public enum Destination: Hashable {
+        case home
+    }
+    
+}
