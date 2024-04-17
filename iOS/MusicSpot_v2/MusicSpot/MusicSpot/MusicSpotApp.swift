11aef10b024cc06bd38abaa0d1e1ec54dad46d3e
Junnos <junnos.dev@gmail.com>
2024-04-17 23:43:42 +0900

    :sparkles: AppDelegate와 SceneDelegate를 추가하고 커스텀 폰트 등록

--- iOS/MusicSpot_v2/MusicSpot/MusicSpot/MusicSpotApp.swift
+++ iOS/MusicSpot_v2/MusicSpot/MusicSpot/MusicSpotApp.swift
@@ -7,13 +7,48 @@
 
 import SwiftUI
 
+import Home
+import MSDesignSystem
+
 @main
 struct MusicSpotApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
+    }
+    
+}
+
+@MainActor
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    // Register SceneDelegate to UIScene
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        if connectingSceneSession.role == .windowApplication {
+            configuration.delegateClass = SceneDelegate.self
+        }
+        return configuration
+    }
+    
+}
+
+@MainActor
+final class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        MSFont.registerFonts()
     }
     
 }
