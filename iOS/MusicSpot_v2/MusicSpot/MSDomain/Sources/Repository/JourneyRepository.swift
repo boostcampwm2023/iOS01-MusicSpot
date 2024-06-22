27f23b4c864b00613ad5bd1910e278cabdcea29f
Junnos <junnos.dev@gmail.com>
2024-06-23 02:54:23 +0900

    ✨ `endJourney` 메서드 삭제
    `updateJourney`로 통합 사용

--- iOS/MusicSpot_v2/MusicSpot/MSDomain/Sources/Repository/JourneyRepository.swift
+++ iOS/MusicSpot_v2/MusicSpot/MSDomain/Sources/Repository/JourneyRepository.swift
@@ -27,8 +27,6 @@ public protocol JourneyRepository {
     /// - Returns: 업데이트 된 여정에 대한 정보를 담은 인스턴스
     @discardableResult
     func updateJourney(_ journey: Journey) async throws(JourneyError) -> Journey
-    @discardableResult
-    func endJourney(_ journey: Journey, isCancelled: Bool) async throws(JourneyError) -> Journey
 
     /// 여정을 삭제합니다.
     /// - Parameters:
@@ -38,11 +36,4 @@ public protocol JourneyRepository {
     // TODO: SwiftLint Swift 6 적용 후 삭제
     // swiftlint:disable:next identifier_name
     func deleteJourney(_ journey: Journey) async throws(JourneyError) -> Journey
-}
-
-extension JourneyRepository {
-    @discardableResult
-    public func endJourney(_ journey: Journey, isCancelled: Bool = false) async throws(JourneyError) -> Journey {
-        return try await self.endJourney(journey, isCancelled: isCancelled)
-    }
 }
